8ee728f818bfb492e1bc53f9c0ecf82cfda84a2c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-10 12:51:51 +0200

    Added file

---
+++ basicSyntaxStatementsLoops/info.js
@@ -0,0 +1,10 @@
+function solve(name, age, grade){
+
+    let studentName = name;
+    let studentAge = age;
+    let avGrade = grade.toFixed(2);
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${avGrade}`);
+
+}
+
+solve('Steve', 16, 2.1426);
