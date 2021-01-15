60c7a35ead05bc2c12622a7ae0693502bf56b5a1
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-15 20:40:40 +0200

    Added file

---
+++ dataTypesAndVariablesExe/calculator.js
@@ -0,0 +1,22 @@
+function solve(num1, operator, num2){
+
+    let result = 0;
+    if(operator == '+'){
+        result = num1 + num2;
+    } else if(operator == '-'){
+        result = num1 - num2;
+    } else if ( operator == '*'){
+        result = num1 * num2;
+    } else if (operator == '/'){
+        result = num1 / num2;
+    }
+
+    console.log(result.toFixed(2));
+
+
+}
+
+solve(25.5,
+    '-',
+    3
+    );
