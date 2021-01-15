3610fb87bfa6e2df543778cddb50a7c949b0787c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-15 20:48:00 +0200

    Added file

---
+++ dataTypesAndVariablesExe/lowerUpper.js
@@ -0,0 +1,14 @@
+function solve(char1) {
+
+    let newChar = char1.toUpperCase();
+
+    if (char1 === newChar) {
+        console.log('upper-case');
+    } else{
+        console.log('lower-case');
+    }
+
+
+}
+
+solve('f')
