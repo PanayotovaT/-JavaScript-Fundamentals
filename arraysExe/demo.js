b0dcbd463e940ad286c8f787193086d17685fb7b
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-17 11:02:10 +0200

    Added file

---
+++ arraysExe/demo.js
@@ -0,0 +1,25 @@
+let arr = [1, 3, 5];
+arr.shift(0);
+
+console.log(arr);
+
+
+let newArr = [1, 6, 7, 8, 9, 20];
+let item =newArr.includes(7);
+
+console.log(item);
+
+let string = 'absd';
+let arrString = string.split('');
+
+console.log(arrString);
+
+for(let element of arr){
+    console.log(element);
+}
+
+let arrTwo = [1, 7, 13, 25, 2, 67];
+let myPop = arrTwo.pop();
+arrTwo.push(17);
+console.log(arrTwo);
+
