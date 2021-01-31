0c2f179e9ff1d1dbf1f9a068d5044d032f7ad908
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-01-31 20:32:21 +0200

    Added file

---
+++ associativeArraysExe/cards.js
@@ -0,0 +1,56 @@
+function solve(input) {
+
+    const colors = {
+        'S': 4,
+        'H': 3, 
+        'D': 2, 
+        'C': 1
+    }
+
+    const cards = {
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
+    }
+
+    let players = {};
+    for(let line of input) {
+        let [name, hand] = line.split(': ');
+        hand = hand.split(', ');
+
+        if(!players.hasOwnProperty(name)) {
+            players[name] = [];
+        }
+        players[name].push(...hand);
+    }
+
+    for(let player in players) {
+
+        let cardsArr = new Set(players[player]);
+        let total = 0;
+        Array.from(cardsArr).forEach(card => {
+            let elements = card.split('');
+            let color = elements.pop();
+            let cardNum = elements.join('');
+            if(isNaN(cardNum)) {
+                cardNum = cards[cardNum];
+            } 
+           total += colors[color] * Number(cardNum);
+
+        })
+        players[player] = total;
+        console.log((`${player}: ${players[player]}`));
+    }
+
+
+}
+solve([
+    'Peter: 2C, 4H, 9H, AS, QS',
+    'Tomas: 3H, 10S, JC, KD, 5S, 10S',
+    'Andrea: QH, QC, QS, QD',
+    'Tomas: 6H, 7S, KC, KD, 5S, 10C',
+    'Andrea: QH, QC, JS, JD, JC',
+    'Peter: JD, JD, JD, JD, JD, JD'
+    ]
+    )
