9d1de614095bdf266265ef833f1fa16c1a1824ff
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-02-04 17:56:28 +0200

    Added file

---
+++ textProcessingLab/demo.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    for(let letter of input) {
+        console.log(letter);
+    }
+
+}
+solve('AWord')
+
+let str = '  \n\n  abs   ghkhd  kjljl   \n\n';
+console.log(str + '<-');
+console.log(str.trim() + '<-');
