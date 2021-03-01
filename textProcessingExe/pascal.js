ef46f05f49c75c904ff1b456959fa9f9fd3ce0ec
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-03-01 17:52:27 +0200

    Added file

---
+++ textProcessingExe/pascal.js
@@ -0,0 +1,14 @@
+function solve(str) {
+    let result = [];
+    let cap = 0;
+    for(let i = 1; i < str.length; i++) {
+        if(str[i].charCodeAt() >= 65 && str[i].charCodeAt() <= 90) {
+            let word = str.substring(cap, i);
+            result.push(word)
+            cap = i
+        }
+    }
+    result.push(str.substring(cap, str.length))
+    console.log(result.join(', '));
+}
+solve('SplitMeIfYouCanHaHaYouCantOrYouCan');
