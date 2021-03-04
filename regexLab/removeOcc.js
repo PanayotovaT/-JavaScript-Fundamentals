9f095393c3261135af5304c9f673f138861be249
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-03-04 20:30:23 +0200

    Added file

---
+++ regexLab/removeOcc.js
@@ -0,0 +1,11 @@
+function solve(word, text) {
+
+    while(text.includes(word)) {
+        
+        text = text.replace(word, '');
+    }
+    console.log(text);
+
+
+}
+solve('ice', 'kicegiciceeb')
