317eb3b3ea5f376ba15ff73e85492d4240774893
fortalesi <75318821+fortalesi@users.noreply.github.com>
2021-03-16 22:34:47 +0200

    Added file

---
+++ dataTypeExe/binary.js
@@ -0,0 +1,13 @@
+function binary(bin){
+
+  
+    // // binary to decimal
+    // ConvertBase.bin2dec = function (num) {
+    //     return ConvertBase(num).from(2).to(10);
+    // };
+
+   console.log(output);
+  
+}
+
+binary(00001001)
